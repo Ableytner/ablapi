b2c68c89f1e64b21e86caeba94741d08838667fe
Ableytner <emanuel.ableitner@gmx.at>
2026-08-24 14:14:14 +0200

    Delete old python code, add Status endpoint

---
+++ Solution/Source/AblApi.Api/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using AblApi.Common.Attributes;
+using AblApi.Common.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AblApi.Api.Controllers;
+
+[Route("api/[controller]/")]
+[ApiController]
+public class StatusController : ControllerBase
+{
+    [HttpGet]
+    [AccessLevel(AccessLevelType.PublicInternetAccess)]
+    public async Task<ActionResult<string>> Index()
+    {
+        return Ok("OK");
+    }
+}
